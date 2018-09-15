d3d1a1c3c435d740a99d2daed7a163e143e45681
Alexander Gorishnyak <kefir500@gmail.com>
2018-09-15 17:32:59 +0600

    Add Debian packaging script

---
+++ scripts/linux/installers/deb/build.deb.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DEBEMAIL="kefir500@gmail.com"
+export DEBFULLNAME="Alexander Gorishnyak"
+VERSION=$(cat ../../../../VERSION)
+
+clean() {
+    rm -rf apk-editor-studio-$VERSION 2> /dev/null
+    rm apk-editor-studio_$VERSION*.tar.xz apk-editor-studio_$VERSION*.dsc apk-editor-studio_$VERSION*.changes apk-editor-studio_$VERSION*.build 2> /dev/null
+}
+
+clean
+rm apk-editor-studio_$VERSION*.deb 2> /dev/null
+mkdir apk-editor-studio-$VERSION
+cp ../../../../*.pro apk-editor-studio-$VERSION/
+cp ../../../../*.pri apk-editor-studio-$VERSION/
+cp -R ../../../../lib apk-editor-studio-$VERSION/
+cp -R ../../../../res apk-editor-studio-$VERSION/
+cp -R ../../../../src apk-editor-studio-$VERSION/
+cp -R debian apk-editor-studio-$VERSION/
+cd apk-editor-studio-$VERSION
+
+dh_make -y -s -c gpl3 --createorig
+debuild -uc -us
+
+cd ..
+clean
