f9d45267e95ab608444dc7b70b4ad7f0eac41e0b
Alexander Gorishnyak <kefir500@gmail.com>
2018-09-17 18:51:53 +0600

    Add Linux binaries archiving script

---
+++ scripts/linux/installers/tar.xz/build.tar.xz.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+if [ -z "$VERSION" ]; then VERSION=$(cat ../../../../VERSION); fi
+cp -R ../../../../bin/linux apk-editor-studio_linux_$VERSION
+tar cfJ apk-editor-studio_$VERSION.tar.xz apk-editor-studio_$VERSION
+rm -rf apk-editor-studio_$VERSION
