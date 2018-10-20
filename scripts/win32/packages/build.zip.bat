a72d2f09c9d6a1f532b13e8adf941b2714a39bb9
Alexander Gorishnyak <kefir500@gmail.com>
2018-10-21 00:08:39 +0600

    Fix zipped folder name generated by CI

--- scripts/win32/packages/build.zip.bat
+++ scripts/win32/packages/build.zip.bat
@@ -1,6 +1,10 @@
 call ..\environment.bat
 
-set FOLDER="APK Editor Studio v%VERSION%"
+if /i not "%VERSION%"=="dev" (
+    set FOLDER="APK Editor Studio v%VERSION%"
+) else (
+    set FOLDER="APK Editor Studio - Dev"
+)
 
 del apk-editor-studio_win32_%VERSION%.zip
 rmdir /s /q %FOLDER%
