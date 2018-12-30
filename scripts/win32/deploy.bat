f76a5d684756e25d7324007426666c534a4cfe8a
Alexander Gorishnyak <kefir500@gmail.com>
2018-12-30 14:11:00 +0600

    Add SVG support on Windows

--- scripts/win32/deploy.bat
+++ scripts/win32/deploy.bat
@@ -5,11 +5,14 @@ rem Deploy Qt Libraries
 xcopy /y "%QTDIR%\bin\Qt5Core.dll" .
 xcopy /y "%QTDIR%\bin\Qt5Gui.dll" .
 xcopy /y "%QTDIR%\bin\Qt5Network.dll" .
+xcopy /y "%QTDIR%\bin\Qt5Svg.dll" .
 xcopy /y "%QTDIR%\bin\Qt5Widgets.dll" .
 xcopy /y "%QTDIR%\bin\Qt5Xml.dll" .
 xcopy /y "%QTDIR%\plugins\imageformats\qgif.dll" imageformats\
+xcopy /y "%QTDIR%\plugins\imageformats\qicns.dll" imageformats\
 xcopy /y "%QTDIR%\plugins\imageformats\qico.dll" imageformats\
 xcopy /y "%QTDIR%\plugins\imageformats\qjpeg.dll"\imageformats\
+xcopy /y "%QTDIR%\plugins\imageformats\qsvg.dll" imageformats\
 xcopy /y "%QTDIR%\plugins\platforms\qwindows.dll" platforms\
 xcopy /y "%QTDIR%\plugins\styles\qwindowsvistastyle.dll" styles\
 
