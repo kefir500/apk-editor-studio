ec297c8dcd81bb6b3cd4ba5f0412a3cf8274bd66
Alexander Gorishnyak <kefir500@gmail.com>
2019-04-23 12:19:00 +0600

    Update Qt to 5.12.2 on Windows

--- scripts/win32/environment.bat
+++ scripts/win32/environment.bat
@@ -1,7 +1,7 @@
 rem The following lines set the paths to the tools required for build.
 rem Feel free to change them to correspond the paths on your system.
 
-if not defined QTDIR        set QTDIR=C:\Qt\Qt5.12.1\5.12.1\msvc2017
+if not defined QTDIR        set QTDIR=C:\Qt\Qt5.12.2\5.12.2\msvc2017
 if not defined OPENSSL      set OPENSSL=C:\OpenSSL\msvc2017
 if not defined VCVARS       set VCVARS=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat
 if not defined VCREDIST_DLL set VCREDIST_DLL=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.16.27012\x86\Microsoft.VC141.CRT
