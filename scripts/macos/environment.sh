9a9c254ae5b6cfa87b3199407f6e8a2ed6cd2603
Alexander Gorishnyak <kefir500@gmail.com>
2019-07-16 11:00:38 +0600

    Update Qt environment paths

--- scripts/macos/environment.sh
+++ scripts/macos/environment.sh
@@ -1,2 +1,2 @@
-if [ -z "$CI" ]; then PATH=$PATH:~/Qt5.*/5.*/clang_64; fi
+if [ -z "$CI" ]; then PATH=$PATH:~/Qt5.6.3/5.6.3/clang_64; fi
 if [ -z "$VERSION" ]; then VERSION=$(cat $(dirname "$BASH_SOURCE")/../../VERSION); fi
