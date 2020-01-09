b01a51b50ac2ce76719204b2ca39b07d158a808d
Alexander Gorishnyak <kefir500@gmail.com>
2020-01-09 11:00:47 +0600

    Add QFileIconProvider proxy to IconProvider

--- src/base/iconprovider.cpp
+++ src/base/iconprovider.cpp
@@ -2,22 +2,32 @@
 #include "base/application.h"
 #include <QDirIterator>
 
-QIcon IconProvider::get(const QString &name)
+QIcon IconProvider::get(const QString &iconName)
 {
     // Find existing icon in a pool:
-    auto existing = pool.value(name);
+    auto existing = pool.value(iconName);
     if (!existing.isNull()) {
         return existing;
     }
 
     // Add new icon to a pool:
     QIcon icon;
-    QDirIterator it(app->getSharedPath("resources/icons"), {name}, QDir::Files, QDirIterator::Subdirectories);
+    QDirIterator it(app->getSharedPath("resources/icons"), {iconName}, QDir::Files, QDirIterator::Subdirectories);
     while (it.hasNext()) {
         icon.addFile(it.next());
     }
     if (!icon.isNull()) {
-        pool.insert(name, icon);
+        pool.insert(iconName, icon);
     }
     return icon;
+}
+
+QIcon IconProvider::get(const QFileInfo &fileInfo) const
+{
+    return fileIconProvider.icon(fileInfo);
+}
+
+QIcon IconProvider::get(QFileIconProvider::IconType iconType) const
+{
+    return fileIconProvider.icon(iconType);
 }
--- src/base/iconprovider.h
+++ src/base/iconprovider.h
@@ -3,14 +3,19 @@
 
 #include <QMap>
 #include <QIcon>
+#include <QFileInfo>
+#include <QFileIconProvider>
 
 class IconProvider
 {
 public:
-    QIcon get(const QString &name);
+    QIcon get(const QString &iconName);
+    QIcon get(const QFileInfo &fileInfo) const;
+    QIcon get(QFileIconProvider::IconType iconType) const;
 
 private:
     QMap<QString, QIcon> pool;
+    QFileIconProvider fileIconProvider;
 };
 
 #endif // ICONPROVIDER_H
