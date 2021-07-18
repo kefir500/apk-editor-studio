1b7f32180f67a60697fe1b9ffa747cd26eab0ac4
Alexander Gorishnyak <kefir500@gmail.com>
2021-07-18 22:10:42 +0600

    Rename APK cloning action

--- src/windows/mainwindow.cpp
+++ src/windows/mainwindow.cpp
@@ -420,7 +420,8 @@ void MainWindow::retranslate()
     //: The "&" is a shortcut key prefix, not an "and" conjunction. Details: https://github.com/kefir500/apk-editor-studio/wiki/Translation-Guide#shortcuts
     actionPermissionEditor->setText(tr("Edit Application &Permissions"));
     //: The "&" is a shortcut key prefix, not an "and" conjunction. Details: https://github.com/kefir500/apk-editor-studio/wiki/Translation-Guide#shortcuts
-    actionPackageRename->setText(tr("Edit Package &Name"));
+    tr("Edit Package &Name"); // For possible future use
+    actionPackageRename->setText(tr("&Clone APK"));
     //: The "&" is a shortcut key prefix, not an "and" conjunction. Details: https://github.com/kefir500/apk-editor-studio/wiki/Translation-Guide#shortcuts
     actionViewSignatures->setText(tr("View &Signatures"));
 
