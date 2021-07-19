acd7d2008f5631c26388846de8c4606779d2ef27
Alexander Gorishnyak <kefir500@gmail.com>
2021-07-20 03:04:41 +0600

    Hide single-window mode setting on macOS

--- src/windows/optionsdialog.cpp
+++ src/windows/optionsdialog.cpp
@@ -203,7 +203,9 @@ void OptionsDialog::initialize()
     spinboxRecent = new QSpinBox(this);
     spinboxRecent->setMinimum(0);
     spinboxRecent->setMaximum(50);
+#ifndef Q_OS_OSX
     pageGeneral->addRow(checkboxSingleInstance);
+#endif
     pageGeneral->addRow(checkboxUpdates);
     pageGeneral->addRow(tr("Language:"), comboLanguages);
     pageGeneral->addRow(tr("Maximum recent files:"), spinboxRecent);
